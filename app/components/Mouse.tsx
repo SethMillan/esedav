07c1f26c4306d777b352dd2daecbfaf2286cb112
Seth Millán <millanseth@gmail.com>
2026-01-11 01:35:14 -0600

    feat: :art: mouse icono jaja esta chido prro

---
+++ app/components/Mouse.tsx
@@ -0,0 +1,10 @@
+
+function Mouse() {
+  return (
+    <div className='border w-6 h-10 rounded-full bg-white border-primary/70'>
+        <div className='w-1 h-2.5 bg-primary rounded-full mx-auto mt-2'></div>
+    </div>
+  )
+}
+
+export default Mouse
